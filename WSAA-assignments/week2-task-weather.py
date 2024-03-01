f7e7794a91f04e03244f3d9d61119ee3491cde1b
Rebecca Hannah Quinn <rebeccahannahquinn@gmail.com>
2024-03-01 17:16:29 +0000

    assignment 2 testing

--- WSAA-assignments/week2-task-weather.py
+++ WSAA-assignments/week2-task-weather.py
@@ -3,21 +3,26 @@
 # python program call currentweather.py that prints out current temperature and wind direction only in current location
 import requests
 import json
+
 from geopy.geocoders import Nominatim
 
 #location to get results from
 latitude = 52.27021579560998
 longitude = -9.707456054277275
 
-url = f"https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&current=temperature_2m,wind_speed_10m"
-response = requests.get(url)
+geolocator = Nominatim(user_agent="geoweather.json")
+location = geolocator.reverse(latitude, longitude)
+print(location.address)
+
+#url = f"https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&current=temperature_2m,wind_speed_10m"
+#response = requests.get(url)
 #print(response.json())
-data = response.json()
+#data = response.json()
 #with open ("currentweather.json", "w") as fp:
            #json.dump(data, fp)
            
-temp = data["current_units"]
-print("temperature_2m")
+#temp = data["current_units"]
+#print("temperature_2m")
 
 
-print("")
+#print("")
