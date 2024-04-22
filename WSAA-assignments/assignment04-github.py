e84fbe40ca07f4329de946b4845a4823b303ba67
Rebecca Hannah Quinn <rebeccahannahquinn@gmail.com>
2024-04-22 19:18:55 +0100

    refresh after importing modules to us

---
+++ WSAA-assignments/assignment04-github.py
@@ -0,0 +1,8 @@
+#module imports
+
+import requests
+
+#must pip install config and github on your device if you do not have it in order for the below 2 module imports to function
+
+from config import config as cfg 
+from github import Github
