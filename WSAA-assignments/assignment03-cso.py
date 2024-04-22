4a2722abb3508ffa0c76063982a80d9e9d911b30
Rebecca Hannah Quinn <rebeccahannahquinn@gmail.com>
2024-04-22 19:09:52 +0100

    assignment 03 commit

--- WSAA-assignments/assignment03-cso.py
+++ WSAA-assignments/assignment03-cso.py
@@ -1,18 +1,13 @@
 #link to data "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/1.0/en"
-import json
 import requests
+import json
 
 url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/1.0/en"
 
-#get dataset from link using function getdata()
-def getdata():
+def getexcheq():
     response = requests.get(url)
-    data = response.json()
-    print(response)
+    return response.json()
     with open("cso.json", "wt") as fp:
-        print(json.dumps(data), file = fp)
-
-#call function
-getdata()
-    
-
+        print(json.dumps(getexcheq()), file = fp)
+             
+getexcheq()
